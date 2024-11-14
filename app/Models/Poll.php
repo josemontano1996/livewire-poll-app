12ca655c6088e92ea4c4fc3961931804d0ed0c93
Josema <jm3develop@gmail.com>
2024-11-14 18:09:14 +0100

    wrote migrations and initial models

--- app/Models/Poll.php
+++ app/Models/Poll.php
@@ -2,9 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Poll extends Model
 {
-    //
+    use HasFactory;
+
+
+    public function option():HasMany
+    {
+        return $this->hasMany(Option::class);
+    }
 }
--- database/migrations/2024_11_14_164159_create_options_table.php
+++ database/migrations/2024_11_14_164159_create_options_table.php
@@ -1,11 +1,11 @@
 <?php
 
+use App\Models\Poll;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,7 +13,11 @@ return new class extends Migration
     {
         Schema::create('options', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+
             $table->timestamps();
+
+            $table->foreignIdFor(Poll::class, 'poll_id')->constrained()->cascadeOnDelete();
         });
     }
 
